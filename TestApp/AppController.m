49222b646e42e594e954c27c129c963300ba9c31
Lars Steiger <lars@fastspring.com>
2010-03-10 17:25:54 +0000

    Using userDefaults to remember entries inside the settings tab after application restart. Renamed FstSpgEmbeddedStore FsprgEmbeddedStore.

--- TestApp/AppController.m
+++ TestApp/AppController.m
@@ -9,6 +9,7 @@
 #import "AppController.h"
 #import "OrderViewController.h"
 
+static NSString * const kParams = @"params";
 
 @implementation AppController
 
@@ -16,22 +17,28 @@
 {
 	self = [super init];
 	if (self != nil) {
-		[self setStoreController:[[[FstSpgEmbeddedStoreController alloc] init] autorelease]];
-		[self setParams:[FstSpgStoreParameters parameters]];
-		[[self params] setStoreId:@"spootnik" withProductId:@"fsembeddedstore"];
-		[[self params] setMode:kFstSpgModeTest];
+		[self setStoreController:[[[FsprgEmbeddedStoreController alloc] init] autorelease]];
+
+		NSDictionary *defaultParams = [[[NSUserDefaultsController sharedUserDefaultsController] defaults] dictionaryForKey:kParams];
+		if(defaultParams == nil) {
+			[self setParams:[FsprgStoreParameters parameters]];
+			[[self params] setOrderProcessType:kFsprgOrderProcessDetail];
+			[[self params] setMode:kFsprgModeTest];
+		} else {
+			[self setParams:[FsprgStoreParameters parametersWithRaw:(NSMutableDictionary *)defaultParams]];
+		}
 		
 		[[self storeController] setDelegate:self];
 	}
 	return self;
 }
 
-- (FstSpgEmbeddedStoreController *)storeController
+- (FsprgEmbeddedStoreController *)storeController
 {
     return [[storeController retain] autorelease]; 
 }
 
-- (void)setStoreController:(FstSpgEmbeddedStoreController *)aStoreController
+- (void)setStoreController:(FsprgEmbeddedStoreController *)aStoreController
 {
     if (storeController != aStoreController) {
         [storeController release];
@@ -39,12 +46,12 @@
     }
 }
 
-- (FstSpgStoreParameters *)params
+- (FsprgStoreParameters *)params
 {
     return [[params retain] autorelease]; 
 }
 
-- (void)setParams:(FstSpgStoreParameters *)aParams
+- (void)setParams:(FsprgStoreParameters *)aParams
 {
     if (params != aParams) {
         [params release];
@@ -74,23 +81,21 @@
 {
 	[previewWebView setHidden:TRUE];
 	[[self storeController] loadWithParameters:[self params]];
-	
-	//NSData *data = [NSData dataWithContentsOfFile:@"/Users/lars/Desktop/test.plist"];
-	//[[previewWebView mainFrame] loadData:data MIMEType:@"text/xml" textEncodingName:@"UTF-8" baseURL:nil];
+	//[[self storeController] loadWithContentsOfFile:@"/Users/lars/Desktop/test.plist"];
 }
 
-// FstSpgEmbeddedStoreDelegate
+// FsprgEmbeddedStoreDelegate
 
 - (void)didLoadStore
 {
 	[previewWebView setHidden:FALSE];
 }
 
-- (void)didReceiveOrder:(FstSpgOrder *)order
+- (void)didReceiveOrder:(FsprgOrder *)order
 {
 }
 
-- (NSView *)viewWithFrame:(NSRect)frame forOrder:(FstSpgOrder *)order
+- (NSView *)viewWithFrame:(NSRect)frame forOrder:(FsprgOrder *)order
 {
 	NSString *errorDesc = nil;
 	NSData *data = [NSPropertyListSerialization dataFromPropertyList:[order raw] 
@@ -101,8 +106,17 @@
 	OrderViewController *orderViewController = [[OrderViewController alloc] initWithNibName:@"OrderView" bundle:nil];
 	[orderViewController setRepresentedObject:orderAsStr];
 
+	[[[orderViewController view] window] setBackgroundColor:[NSColor blackColor]];
 	[[orderViewController view] setFrame:frame];
 	return [orderViewController view];
+}
+
+// NSApplicationDelegate
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
+	NSUserDefaults *defaults = [[NSUserDefaultsController sharedUserDefaultsController] defaults];
+	[defaults setObject:[[self params] raw] forKey:kParams];
 }
 
 - (void)dealloc
