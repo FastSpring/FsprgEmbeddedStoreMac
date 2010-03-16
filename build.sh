93ef676b59794ce3fd4a64a102a69e90abf33ce4
Lars Steiger <lars@fastspring.com>
2010-03-16 22:06:37 +0000

    Added documentation (README).

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ rm -R -f FsprgEmbeddedStoreSDK
 ./build_project.sh Example1 Release FsprgEmbeddedStoreSDK copy_src
 ./build_project.sh Example2 Release FsprgEmbeddedStoreSDK copy_src
 
+cp -R ./README ./FsprgEmbeddedStoreSDK
+perl ./Markdown_1.0.1/Markdown.pl --html4tags README.mdown > ./FsprgEmbeddedStoreSDK/README.html
+
 tar -zcf FsprgEmbeddedStoreSDK.tar.gz FsprgEmbeddedStoreSDK
 rm -R -f FsprgEmbeddedStoreSDK
