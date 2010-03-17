09d6d8530b9912ce39bb666994903b688ceac984
Lars Steiger <lars@fastspring.com>
2010-03-17 17:12:30 +0000

    Improved documentation and don't deliver binaries for Example1 and Example2 anymore.

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 rm -R -f FsprgEmbeddedStoreSDK
-./build_project.sh FsprgEmbeddedStore Release FsprgEmbeddedStoreSDK copy_src
-./build_project.sh TestApp Release FsprgEmbeddedStoreSDK
-./build_project.sh Example1 Release FsprgEmbeddedStoreSDK copy_src
-./build_project.sh Example2 Release FsprgEmbeddedStoreSDK copy_src
+./build_project.sh "FsprgEmbeddedStore" "Release 10.4" "FsprgEmbeddedStoreSDK/MacOS 10.5+"
+./build_project.sh "FsprgEmbeddedStore" "Release 10.5+" "FsprgEmbeddedStoreSDK/MacOS 10.4"
+./build_project.sh "TestApp" "Release" "FsprgEmbeddedStoreSDK"
+./build_project.sh "FsprgEmbeddedStore" "Source" "FsprgEmbeddedStoreSDK/src"
+./build_project.sh "Example1" "Source" "FsprgEmbeddedStoreSDK/src"
+./build_project.sh "Example2" "Source" "FsprgEmbeddedStoreSDK/src"
 
-cp -R ./README ./FsprgEmbeddedStoreSDK
-perl ./Markdown_1.0.1/Markdown.pl --html4tags README.mdown > ./FsprgEmbeddedStoreSDK/README.html
+mkdir -p FsprgEmbeddedStoreSDK/README
+cp -R -f README/* FsprgEmbeddedStoreSDK/README
+perl ./Markdown_1.0.1/Markdown.pl --html4tags README.mdown >> ./FsprgEmbeddedStoreSDK/README.html
+cp -R License.txt FsprgEmbeddedStoreSDK
 
 tar -zcf FsprgEmbeddedStoreSDK.tar.gz FsprgEmbeddedStoreSDK
 rm -R -f FsprgEmbeddedStoreSDK
--- build_project.sh
+++ build_project.sh
@@ -1,36 +1,32 @@
 export PRODUCT_NAME=$1
 export CONFIGURATION=$2
 export TARGET_DIR=$3
-export COPY_SRC=$4
 
-xcodebuild -project $PRODUCT_NAME/$PRODUCT_NAME.xcodeproj -target $PRODUCT_NAME -configuration $CONFIGURATION clean build
-
-echo "Copy binaries to $TARGET_DIR"
 if [ ! -d "$TARGET_DIR" ]; then
-	mkdir $TARGET_DIR
-fi
-export APP_DIR=$PRODUCT_NAME/build/$CONFIGURATION/$PRODUCT_NAME.app
-if [ -d "$APP_DIR" ]; then
-	cp -R -f $APP_DIR $TARGET_DIR
-fi
-export FRAMEWORK_DIR=$PRODUCT_NAME/build/$CONFIGURATION/$PRODUCT_NAME.framework
-if [ -d "$FRAMEWORK_DIR" ]; then
-	cp -R -f $FRAMEWORK_DIR $TARGET_DIR
+	mkdir -p "$TARGET_DIR"
 fi
 
-if [[ $COPY_SRC ]]; then
-	export TARGET_SRC_DIR=$TARGET_DIR/src
-	echo "Copy source to $TARGET_SRC_DIR"
-	if [ ! -d "$TARGET_SRC_DIR" ]; then
-		mkdir $TARGET_SRC_DIR
-	fi
+if [[ $CONFIGURATION = "Source" ]]; then
+	echo "Copy source of $PRODUCT_NAME to $TARGET_DIR"
 	
 	# copy src
-	cp -R -f $PRODUCT_NAME $TARGET_SRC_DIR
+	cp -R -f "$PRODUCT_NAME" "$TARGET_DIR"
 	# remove build directory
-	rm -R -f $TARGET_SRC_DIR/$PRODUCT_NAME/build
+	rm -R -f "$TARGET_DIR/$PRODUCT_NAME/build"
 	# remove .svn directories
-	find $TARGET_SRC_DIR/$PRODUCT_NAME -name .svn -print0 | xargs -0 rm -rf
+	find "$TARGET_DIR/$PRODUCT_NAME" -name .svn -print0 | xargs -0 rm -rf
 	# remove .DS_Store directories
-	find $TARGET_SRC_DIR/$PRODUCT_NAME -name .DS_Store -print0 | xargs -0 rm -rf
+	find "$TARGET_DIR/$PRODUCT_NAME" -name .DS_Store -print0 | xargs -0 rm -rf
+else
+	xcodebuild -project "$PRODUCT_NAME/$PRODUCT_NAME.xcodeproj" -target "$PRODUCT_NAME" -configuration "$CONFIGURATION" clean build
+
+	echo "Copy binaries of $PRODUCT_NAME to $TARGET_DIR"
+	export APP_DIR=$PRODUCT_NAME/build/$CONFIGURATION/$PRODUCT_NAME.app
+	if [ -d "$APP_DIR" ]; then
+		cp -R -f "$APP_DIR" "$TARGET_DIR"
+	fi
+	export FRAMEWORK_DIR=$PRODUCT_NAME/build/$CONFIGURATION/$PRODUCT_NAME.framework
+	if [ -d "$FRAMEWORK_DIR" ]; then
+		cp -R -f "$FRAMEWORK_DIR" "$TARGET_DIR"
+	fi
 fi
