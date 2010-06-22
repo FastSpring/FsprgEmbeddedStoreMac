3ccc84c76063b36106e32c57e0cb82cead69e589
Lars Steiger <lars@fastspring.com>
2010-06-22 11:22:51 +0000

    Renamed FsprgEmbeddedStoreSDK to FsprgEmbeddedStoreMacSDK.

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-rm -R -f FsprgEmbeddedStoreSDK
-./build_project.sh "FsprgEmbeddedStore" "Release 10.4" "FsprgEmbeddedStoreSDK/MacOS 10.4"
-./build_project.sh "FsprgEmbeddedStore" "Release 10.5+" "FsprgEmbeddedStoreSDK/MacOS 10.5+"
-./build_project.sh "TestApp" "Release" "FsprgEmbeddedStoreSDK"
-./build_project.sh "FsprgEmbeddedStore" "Source" "FsprgEmbeddedStoreSDK/src"
-./build_project.sh "Example1" "Source" "FsprgEmbeddedStoreSDK/src"
-./build_project.sh "Example2" "Source" "FsprgEmbeddedStoreSDK/src"
+rm -R -f FsprgEmbeddedStoreMacSDK
+./build_project.sh "FsprgEmbeddedStore" "Release 10.4" "FsprgEmbeddedStoreMacSDK/MacOS 10.4"
+./build_project.sh "FsprgEmbeddedStore" "Release 10.5+" "FsprgEmbeddedStoreMacSDK/MacOS 10.5+"
+./build_project.sh "TestApp" "Release" "FsprgEmbeddedStoreMacSDK"
+./build_project.sh "FsprgEmbeddedStore" "Source" "FsprgEmbeddedStoreMacSDK/src"
+./build_project.sh "Example1" "Source" "FsprgEmbeddedStoreMacSDK/src"
+./build_project.sh "Example2" "Source" "FsprgEmbeddedStoreMacSDK/src"
 
 # FsprgEmbeddedStoreStyle.zip
 zip -r FsprgEmbeddedStoreStyle FsprgEmbeddedStoreStyle -x *.svn* *.DS_Store*
-mv FsprgEmbeddedStoreStyle.zip FsprgEmbeddedStoreSDK 
+mv FsprgEmbeddedStoreStyle.zip FsprgEmbeddedStoreMacSDK 
 
 # README.html
-mkdir -p FsprgEmbeddedStoreSDK/README
-cp -R -f README/* FsprgEmbeddedStoreSDK/README
-perl ./Markdown_1.0.1/Markdown.pl --html4tags README.mdown >> ./FsprgEmbeddedStoreSDK/README.html
+mkdir -p FsprgEmbeddedStoreMacSDK/README
+cp -R -f README/* FsprgEmbeddedStoreMacSDK/README
+perl ./Markdown_1.0.1/Markdown.pl --html4tags README.mdown >> ./FsprgEmbeddedStoreMacSDK/README.html
 
 # License.txt
-cp -R License.txt FsprgEmbeddedStoreSDK
+cp -R License.txt FsprgEmbeddedStoreMacSDK
 
 # Package and remove temp directory
-tar -zcf FsprgEmbeddedStoreSDK.tar.gz FsprgEmbeddedStoreSDK
-rm -R -f FsprgEmbeddedStoreSDK
+tar -zcf FsprgEmbeddedStoreMacSDK.tar.gz FsprgEmbeddedStoreMacSDK
+rm -R -f FsprgEmbeddedStoreMacSDK
