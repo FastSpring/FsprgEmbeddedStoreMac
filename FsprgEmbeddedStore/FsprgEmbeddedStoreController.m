9ae0929884899987af7ce3654ac7d383b8e7b9ea
Lars Steiger <lars@fastspring.com>
2011-08-03 16:42:45 +0200

    Changed order response mimetype to from text/xml to application/x-fsprgorder+xml. This prevents interference with Safari Plugins like XMLViewPlugin.

--- FsprgEmbeddedStore/FsprgEmbeddedStoreController.m
+++ FsprgEmbeddedStore/FsprgEmbeddedStoreController.m
@@ -26,10 +26,7 @@
 {
 	[WebView registerViewClass:[FsprgOrderView class]
 		   representationClass:[FsprgOrderDocumentRepresentation class]
-				   forMIMEType:@"text/xml"];
-	[WebView registerViewClass:[FsprgOrderView class]
-		   representationClass:[FsprgOrderDocumentRepresentation class]
-				   forMIMEType:@"application/xml"];
+				   forMIMEType:@"application/x-fsprgorder+xml"];
 }
 
 - (id) init
@@ -64,7 +61,7 @@
 			[webView setPostsFrameChangedNotifications:TRUE];
 			[webView setFrameLoadDelegate:self];
 			[webView setUIDelegate:self];
-			[webView setApplicationNameForUserAgent:@"FSEmbeddedStore/1.0"];
+			[webView setApplicationNameForUserAgent:@"FSEmbeddedStore/2.0"];
 			[[NSNotificationCenter defaultCenter] addObserver:self 
 													 selector:@selector(webViewFrameChanged:) 
 														 name:NSViewFrameDidChangeNotification 
@@ -115,8 +112,12 @@
 {
 	[self setStoreHost:nil];
 
-	NSString *fileURL = [NSString stringWithFormat:@"file://%@", aPath];
-	[[webView mainFrame] loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:fileURL]]];
+	NSData *data = [NSData dataWithContentsOfFile:aPath];
+	if(data == nil) {
+		NSLog(@"File %@ not found.", aPath);
+	} else {
+		[[webView mainFrame] loadData:data MIMEType:@"application/x-fsprgorder+xml" textEncodingName:@"UTF-8" baseURL:nil];
+	}
 }
 
 - (BOOL)isLoading
