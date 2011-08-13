ae2b32374a03718c417bf71da28d51d00e6ccf09
Lars Steiger <lars.steiger@buzzard.ch>
2011-08-13 11:53:05 +0200

    Using xcodebuild clean install (instead of clean build) to create the binaries. That way binaries should be stripped and file mode, owner, and group information should be set to standard values. Thus, "Could not find object file ... no debug information available for ..." warnings will disappear.

--- build_project.sh
+++ build_project.sh
@@ -14,19 +14,20 @@ if [[ $CONFIGURATION = "Source" ]]; then
 	# remove development specific files
 	rm $TARGET_DIR/$PRODUCT_NAME/$PRODUCT_NAME.xcodeproj/*.mode1v3
 	rm $TARGET_DIR/$PRODUCT_NAME/$PRODUCT_NAME.xcodeproj/*.pbxuser
-	# remove build directory
+	# remove build and installation directory
 	rm -R -f "$TARGET_DIR/$PRODUCT_NAME/build"
+	rm -R -f "/tmp/$PRODUCT_NAME.dst"
 	# remove .DS_Store directories
 	find "$TARGET_DIR/$PRODUCT_NAME" -name .DS_Store -print0 | xargs -0 rm -rf
 else
-	xcodebuild -project "$PRODUCT_NAME/$PRODUCT_NAME.xcodeproj" -target "$PRODUCT_NAME" -configuration "$CONFIGURATION" clean build
+	xcodebuild -project "$PRODUCT_NAME/$PRODUCT_NAME.xcodeproj" -target "$PRODUCT_NAME" -configuration "$CONFIGURATION" clean install
 
 	echo "Copy binaries of $PRODUCT_NAME to $TARGET_DIR"
-	export APP_DIR=$PRODUCT_NAME/build/$CONFIGURATION/$PRODUCT_NAME.app
+	export APP_DIR=/tmp/$PRODUCT_NAME.dst$HOME/Applications/$PRODUCT_NAME.app
 	if [ -d "$APP_DIR" ]; then
 		cp -R -f "$APP_DIR" "$TARGET_DIR"
 	fi
-	export FRAMEWORK_DIR=$PRODUCT_NAME/build/$CONFIGURATION/$PRODUCT_NAME.framework
+	export FRAMEWORK_DIR=/tmp/$PRODUCT_NAME.dst$HOME/Library/Frameworks/$PRODUCT_NAME.framework
 	if [ -d "$FRAMEWORK_DIR" ]; then
 		cp -R -f "$FRAMEWORK_DIR" "$TARGET_DIR"
 	fi
