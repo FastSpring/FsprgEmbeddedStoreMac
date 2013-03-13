a830c87a345bdfc2d79f28af26a71978521416ab
Lars Steiger <lars@fastspring.com>
2013-03-13 14:50:34 -0300

    Update main.m

--- Example2/main.m
+++ Example2/main.m
@@ -3,7 +3,7 @@
 //  Example2
 //
 //  Created by Lars Steiger on 3/11/10.
-//  Copyright Buzzard AG 2010. All rights reserved.
+//  Copyright 2010 FastSpring. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
