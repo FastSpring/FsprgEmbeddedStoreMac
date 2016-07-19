9c8c9fec1019d7d89c5c413d1926d082bb4322b8
Luc Vandal <luc@edovia.com>
2016-07-19 12:50:06 -0400

    Added Carthage support

--- FsprgEmbeddedStore/FsprgEmbeddedStore.h
+++ FsprgEmbeddedStore/FsprgEmbeddedStore.h
@@ -2,17 +2,18 @@
 //  FsprgEmbeddedStore.h
 //  FsprgEmbeddedStore
 //
-//  Created by Lars Steiger on 2/28/10.
-//  Copyright 2010 FastSpring. All rights reserved.
+//  Created by Luc Vandal on 2016-07-19.
+//  Copyright © 2016 Edovia Inc. All rights reserved.
 //
 
-#import "FsprgEmbeddedStoreController.h"
-#import "FsprgEmbeddedStoreDelegate.h"
-#import "FsprgStoreParameters.h"
+#import <Cocoa/Cocoa.h>
 
-// Model
-#import "FsprgOrder.h"
-#import "FsprgOrderItem.h"
-#import "FsprgFulfillment.h"
-#import "FsprgLicense.h"
-#import "FsprgFileDownload.h"
+//! Project version number for FsprgEmbeddedStore.
+FOUNDATION_EXPORT double FsprgEmbeddedStoreVersionNumber;
+
+//! Project version string for FsprgEmbeddedStore.
+FOUNDATION_EXPORT const unsigned char FsprgEmbeddedStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FsprgEmbeddedStore/PublicHeader.h>
+
+
